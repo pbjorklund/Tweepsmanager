e7ff3cb3a0e3afb062dc2c515ff0c4caf01bb00c
Patrik Björklund <p.bjorklund@gmail.com>
2012-01-07 11:56:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweepsmanager::Application.config.secret_token = '6d4d59d61d2647a4faf9f891daa461af5c62d75b0a9cff8d1acb44cd6412c4da40bd5c7b113f7797526f7d63477fe4d1ce10dcd6c5a71dc2d73cbc9e08db2825'
