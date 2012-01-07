0617d2d45eb8eaa55d9dbbe21cdde7df4ddaac2f
Th3dz <erikskoglund88@gmail.com>
2012-01-07 15:23:15 +0100

    Revert "Fixed root routing"
    This reverts commit 9fe7a3e4b7d40b41432a57a9ce62bcf3eb6c1d04.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tweepsmanager::Application.routes.draw do
+  get "home/index"  root :to => 'home#index'
+  
   match '/auth/:provider/callback', to: 'sessions#create'
   match '/signout', to: "sessions#destroy", as: :signout
   # The priority is based upon order of creation:
