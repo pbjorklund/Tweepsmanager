e6026592ca3eca4e77a259227b58903f585cac6b
Th3dz <erikskoglund88@gmail.com>
2012-01-07 20:11:37 +0100

    Added new ui + new logout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-	
+	if current_user
+		render layout: "start", action: "start"
+	else
+		render layout: "start", action: "start"
+	end
   end
 end
