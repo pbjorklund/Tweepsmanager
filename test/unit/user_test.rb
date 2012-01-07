8985401c6fa49806227c0b03334462d0b375fe0a
Patrik Björklund <p.bjorklund@gmail.com>
2012-01-07 16:08:54 +0100

    User validations and user unittests added

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,10 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def setup
-    OmniAuth.config.test_mode = true
+  setup do
+    @auth = { }
+    @auth['provider'] = 'twitter'
+    @auth['uid'] = '12345'
+    @auth['info'] = {}
+    @auth['info']['name'] = 'Sir Nilson Mandela'
+
+    @bad_auth = {}
   end
 
+  test "it should create a new user with valid auth" do
+    assert_difference('User.count') do
+      User.create_with_omniauth(@auth)
+    end
+  end
+
+  test "it should not create a new user with invalid auth" do
+    assert_no_difference('User.count') do
+      User.create_with_omniauth(@bad_auth)
+    end
+  end
 
 
 end
