0309b9fdcc20b3c66da7204c915a2b04794f1824
Th3dz <erikskoglund88@gmail.com>
2012-01-07 20:33:02 +0100

    ...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
 	if current_user
-		render layout: "start", action: "start"
+		render layout: "application"
 	else
 		render layout: "start", action: "start"
 	end
