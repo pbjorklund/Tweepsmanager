1223ae24608187bcfa88ef03622428d3022a5308
Patrik Björklund <p.bjorklund@gmail.com>
2012-01-07 13:01:52 +0100

    Added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tweepsmanager::Application.routes.draw do
+  match '/auth/:provider/callback', to: 'sessions#create'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
