32bbf2f12991aaa2e813d69ff7bf1f2ae7147c4b
Patrik Björklund <p.bjorklund@gmail.com>
2012-01-07 12:59:22 +0100

    Added api keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :twitter, 'SYxcRw70lHqqHplj4DLRZA', 'q3ybMrQQ7fqwOUv6X9XUJxKqWvF5RoL3FElcM2XQ'
 end
