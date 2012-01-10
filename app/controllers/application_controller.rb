a8b8550da8c2a9d5f50d48665672d50f633f544e
Patrik Björklund <p.bjorklund@gmail.com>
2012-01-10 19:02:31 +0100

    Refactor / added duplication to views
    * Moved twitter interactions to application controller helper methods
    * Added alot of duplication to views, atleast something in them now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user, :twitter
+  helper_method :current_user, :twitter, :get_not_following_user, :get_followers, :get_following
 
   private
   def current_user
@@ -8,6 +8,30 @@ class ApplicationController < ActionController::Base
   end
 
   def twitter
-    Twitter::Client.new(:oauth_token => current_user.token, :oauth_token_secret => current_user.secret)
+    @twitter ||= Twitter::Client.new(:oauth_token => current_user.token, :oauth_token_secret => current_user.secret)
+  end
+
+  def get_users_not_following_back(limit = 100)
+    users = twitter.friend_ids(current_user.nickname).ids.to_set - twitter.follower_ids(current_user.nickname).ids.to_set
+    twitter.users(users.first(limit))
+  end
+
+  def get_followers(limit = 100)
+    follower_ids = twitter.follower_ids(current_user.nickname).ids
+    twitter.users(follower_ids.first(limit))
+  end
+
+  def get_following(limit = 100)
+    following_ids = twitter.friend_ids(current_user.nickname).ids
+    twitter.users(following_ids.first(limit))
+  end
+
+  def get_stalkers(limit = 100)
+    users = twitter.follower_ids(current_user.nickname).ids.to_set - twitter.friend_ids(current_user.nickname).ids.to_set
+    twitter.users(users.first(limit))
+  end
+
+  def get_api_calls_left
+    twitter.rate_limit_status.remaining_hits
   end
 end
