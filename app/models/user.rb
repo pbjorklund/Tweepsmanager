692b4b75509b16b9ecc50c7c436cd9fcd57fea72
Patrik Björklund <p.bjorklund@gmail.com>
2012-01-19 19:14:35 +0100

    Changed from strings to symbols when creating user from omniauth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
   validates_presence_of :provider, :uid, :name
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
-      user.image_url = auth["info"]["image"]
-      user.nickname = auth["info"]["nickname"]
-      user.token = auth["credentials"]["token"]
-      user.secret = auth["credentials"]["secret"]
+      user.provider = auth[:provider]
+      user.uid = auth[:uid]
+      user.name = auth[:info][:name]
+      user.image_url = auth[:info][:image]
+      user.nickname = auth[:info][:nickname]
+      user.token = auth[:credentials][:token]
+      user.secret = auth[:credentials][:secret]
     end
   end
 
@@ -25,6 +25,6 @@ class User < ActiveRecord::Base
       user.save!
     end
     user
-    
+
   end
 end
