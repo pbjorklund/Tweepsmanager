12fea3e618f08767d91be1eac4c52c22f07e72da
Patrik Björklund <p.bjorklund@gmail.com>
2012-01-21 21:39:48 +0100

    Redesign of users table. Show button to follow/unfollow depending on following status

--- app/models/twitter_follower.rb
+++ app/models/twitter_follower.rb
@@ -6,14 +6,13 @@ class TwitterFollower
   attr :current_user
 
   #TODO Move this out to resque
-  #TODO Figure out how to use VCR
   def twitter
     @twitter ||= Twitter::Client.new(:oauth_token => current_user.token, :oauth_token_secret => current_user.secret)
   end
 
   def get_users_not_following_back(limit = 100)
     users = twitter.friend_ids(current_user.nickname).ids.to_set - twitter.follower_ids(current_user.nickname).ids.to_set
-    twitter.users(users.first(limit))
+    u = twitter.users(users.first(limit))
   end
 
   def get_followers(limit = 100)
