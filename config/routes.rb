580a4922757e82f570ddef0a2de8fec98aa19bf6
Patrik Björklund <p.bjorklund@gmail.com>
2012-02-23 17:26:33 +0100

    About page and link to blog

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Tweepsmanager::Application.routes.draw do
 
   match '/auth/:provider/callback', to: 'sessions#create'
   match '/login', to: "home#login", as: :login
+  match '/about', to: "home#about", as: :about
   match '/signout', to: "sessions#destroy", as: :signout
   # The priority is based upon order of creation:
   # first created -> highest priority.
