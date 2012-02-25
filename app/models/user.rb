a558c6eeee4c095890f71204544d6553ab7c27f5
Patrik Björklund <p.bjorklund@gmail.com>
2012-02-25 19:30:54 +0100

    Refactor of user and user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,26 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :image_url, :nickname
-  has_one :auth
+  has_one :auth, dependent: :destroy
 
   def self.create_with_omniauth(auth)
     user = User.find_or_create_by_id(auth[:uid]) do |user|
       user.id        = auth[:uid]
-      user.name      = auth[:info][:name]
-      user.image_url = auth[:info][:image]
-      user.nickname  = auth[:info][:nickname]
-      user.bio       = auth[:info][:description]
-      user.auth = Auth.find_or_create_by_token_and_secret(auth[:credentials][:token], auth[:credentials][:secret]) do |a|
-        a.provider = "twitter"
-        a.token    = auth[:credentials][:token]
-        a.secret   = auth[:credentials][:secret]
-      end
     end
+    user.update_attributes( {
+      name: auth[:info][:name],
+      image_url: auth[:info][:image],
+      nickname: auth[:info][:nickname],
+      bio: auth[:info][:description]
+    } )
+
+    user.auth = Auth.find_or_initialize_by_token_and_secret(auth[:credentials][:token], auth[:credentials][:secret])
+
+    user.auth.update_attributes( {
+      provider: "twitter",
+      token: auth[:credentials][:token],
+      secret: auth[:credentials][:secret]
+    })
+
+    user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,54 +2,66 @@ require 'spec_helper'
 
 describe User do
 
-  describe "when created" do
-    let(:user) { FactoryGirl.create(:pbjorklund) }
-    subject { user }
+  context "creation / destruction" do
+    subject { FactoryGirl.create(:pbjorklund) }
 
-    specify { subject.should be_valid }
-    it "should require a name" do
-      subject.name = nil
-      subject.should be_invalid
+    describe "when created" do
+
+      specify { subject.should be_valid }
+      it "should require a name" do
+        subject.name = nil
+        subject.should be_invalid
+      end
+
+      it "should require a image_url" do
+        subject.image_url = nil
+        subject.should be_invalid
+      end
+
+      it "should require a nickname" do
+        subject.nickname = nil
+        subject.should be_invalid
+      end
     end
 
-    it "should require a image_url" do
-      subject.image_url = nil
-      subject.should be_invalid
-    end
-
-    it "should require a nickname" do
-      subject.nickname = nil
-      subject.should be_invalid
+    describe "when destroyed" do
+      it "destroys the associated auth" do
+        expect { subject.destroy }.to change(Auth, :count).by(0)
+      end
     end
   end
 
   describe "when using omniauth" do
-    before(:each) do
-      @auth = {
-        uid: "123456",
-        :info => { name: "The Name", image: "http://fj.com/image.jpg", nickname: "pbjorklund", description: "rocking it" },
-        :credentials => { token: "tokentoken", secret:"secretsecret" }
-      }
-    end
+    let(:auth) { {
+      uid: "123456",
+      :info => { name: "The Name", image: "http://fj.com/image.jpg", nickname: "pbjorklund", description: "rocking it" },
+      :credentials => { token: "tokentoken", secret:"secretsecret" }
+    } }
 
-    let(:user) { User.create_with_omniauth(@auth) }
-
-    subject { user }
+    subject { User.create_with_omniauth(auth) }
 
     describe "#self.create_with_omniauth" do
-      specify { lambda { subject }.should change(User, :count).by(1) }
-      specify { lambda { subject }.should change(Auth, :count).by(1) }
-      specify { subject.auth.token.should == "tokentoken" }
-
-      it "updates a user when a new hash is sent" do
-        @auth[:info][:description] = "New description"
-        lambda { user }.should_not raise_error
-        subject.bio.should == "New description"
+      context "when creating new user" do
+        specify { expect { subject }.to change(User, :count).by(1) }
+        specify { expect { subject }.to change(Auth, :count).by(1) }
       end
 
-      it "updates the users auth when new credentials is set" do
-        @auth[:credentials][:token] = "newtoken"
-        subject.auth.token.should == "newtoken"
+      context "when updating a user" do
+        before(:each) do
+          subject
+        end
+
+        it "updates a user when a new hash is sent" do
+          auth[:info][:description] = "New description"
+          lambda { User.create_with_omniauth(auth) }.should_not raise_error
+          User.first.bio.should == "New description"
+        end
+
+        it "updates the users auth when new credentials is set" do
+          auth[:credentials][:token] = "newtoken"
+          user = User.create_with_omniauth(auth)
+          Auth.first.token.should == "newtoken"
+        end
       end
     end
   end
