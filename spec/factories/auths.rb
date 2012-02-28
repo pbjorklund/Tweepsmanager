8d291bfacac814702f56d817a1908a7ec8f32539
Patrik Björklund <p.bjorklund@gmail.com>
2012-02-28 19:04:25 +0100

    Updated auth with valid token....

--- spec/factories/auths.rb
+++ spec/factories/auths.rb
@@ -10,4 +10,10 @@ FactoryGirl.define do
     token      FactoryGirl.generate(:token)
     secret     FactoryGirl.generate(:secret)
   end
+
+  factory :real_auth, class: Auth do
+    provider   "twitter"
+    token  "458298205-Pm0TQtBrKOI2aHOT0AXKQEP5JZzyWngSmNunXHzc"
+    secret "CSPVizi0vuf5pFsGbdpYG48SImVV3WYM45ZG0kSeYJE"
+  end
 end
