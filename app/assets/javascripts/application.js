414a0101d75339c7a8810f3c359ead6e3d4d80df
Patrik Björklund <p.bjorklund@gmail.com>
2012-03-08 08:54:31 +0100

    Cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,85 +16,90 @@
 //= require_tree .
 
 //Bootstrap
-$(document).ready(function(){
-	$('.dropdown-toggle').dropdown()
+
+$(document).ready(function () {
+  $('.dropdown-toggle').dropdown();
 });
 
 function initPopover() {
-    $("a[rel=popover]")
-        .popover({
-					offset: 10, live: true
-        })
-        .click(function(e) {
-            e.preventDefault()
-        })
-};
+  $("a[rel=popover]").
+    popover({ offset: 10, live: true }).
+    click(function (e) {
+      e.preventDefault();
+    })
+}
 
 //Google analytics
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-28190889-2']);
 _gaq.push(['_trackPageview']);
 
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+(function () {
+  var ga, s;
+  ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+}());
 
 function loadUsers(path, user, page) {
-  $.ajax({ 
+  $.ajax({
     url: path,
     type: 'get',
-    dataType:'script',
+    dataType: 'script',
     data: "user=" + user + "&page=" + page,
     timeout: 15000,
-    error: function(){
+    error: function () {
       $("#main").prepend('<div class="alert alert-error">Something went wrong.</div>');
       $(".hero-unit").html("<h1 id=\"loading\">Could not load users</h1>");
     },
-    success: function() {
+    success: function () {
       initPopover();
     }
   });
-};
+}
 
 //Pagination
 //Adds a pagination div
 //userPage = current page, numOfPages = total number of pages, path = current path, user = username
 function addPagination(userPage, numOfPages, path, user) {
-  if(numOfPages > 0) {
-    if(userPage > 0) {
+  var i;
+
+  if (numOfPages > 0) {
+    if (userPage > 0) {
       $("div .pagination ul").append('<li id="previous"><a href="#">Prev</a></li>');
     }
 
-    for (i = 0;i<= numOfPages;i++) { 
-      if(userPage === i) {
-        $("div .pagination ul").append('<li class="active page" data-value="' + i + '"><a href="#">' + (i+1) + '</a></li>'); 
+    for (i = 0; i <= numOfPages; i += 1) {
+      if (userPage === i) {
+        $("div .pagination ul").append('<li class="active page" data-value="' + i + '"><a href="#">' + (i + 1) + '</a></li>');
       } else {
-        $("div .pagination ul").append('<li class="page" data-value="' + i + '"><a href="#">' + (i+1) + '</a></li>'); 
+        $("div .pagination ul").append('<li class="page" data-value="' + i + '"><a href="#">' + (i + 1) + '</a></li>');
       }
     }
 
-    if(numOfPages > userPage) {
+    if (numOfPages > userPage) {
       $("div .pagination ul").append('<li id="next"><a href="#">Next</a></li>');
     }
 
-    $(".page").click( function () { 
+    $(".page").click(function () {
       $("#user-table").html('<div class="hero-unit"> <h1 id="loading">Loading users...</h1> </div>');
       loadUsers(path, user, $(this).data().value);
       $('input[type=button]').attr('disabled', true);
     });
 
-    $("#next").click( function () { 
+    $("#next").click(function () {
       $("#user-table").html('<div class="hero-unit"> <h1 id="loading">Loading users...</h1> </div>');
       loadUsers(path, user, (userPage + 1));
       $('input[type=button]').attr('disabled', true);
     });
 
-    $("#previous").click( function () { 
+    $("#previous").click(function () {
       $("#user-table").html('<div class="hero-unit"> <h1 id="loading">Loading users...</h1> </div>');
       loadUsers(path, user, (userPage - 1));
       $('input[type=button]').attr('disabled', true);
     });
-  };
-};
+  }
+}
